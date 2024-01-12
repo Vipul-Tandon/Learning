d3c594ac0f8a29b063f118f7b6e7530771db8bdb
Vipul-Tandon <vipul.tandon@metafic.co>
2024-01-12 16:31:24 +0530

    Practiced hash

---
+++ Ruby/Day 3/6.hash.rb
@@ -0,0 +1,44 @@
+# Creating hash
+## =>
+grades = { "Jane Doe" => 10, "Jim Doe" => 6 }
+## :
+options = { font_size: 10, font_family: "Arial" }
+puts options[:font_size]
+
+
+# Comparing hash
+h1 = { "a" => 1, "c" => 2 }
+h2 = { 7 => 35, "c" => 2, "a" => 1 }
+h3 = { "a" => 1, "c" => 2, 7 => 35 }
+h4 = { "a" => 1, "d" => 2, "f" => 35 }
+puts h1 == h2   #=> false
+puts h2 == h3   #=> true
+puts h3 == h4   #=> false
+
+
+# Adding new value
+puts h1
+h1.store("b", 3)
+puts h1
+
+
+# Iterating over values
+# Can iterate through the keys and values of a hash using methods like each, each_key, and each_value.
+h1.each do |key, value|
+    print "[#{key}, #{value}] "
+end
+puts
+
+
+# Altering hash
+## .invert: keys become values & values become keys
+h = { "pen": 1, "cap": 2, "bottle": 3}
+puts h
+hh = h.invert
+puts hh
+puts hh[1]
+
+
+# Searching
+## searching a key from its value
+puts h.key(2)
