213b2df08dd2bdda3ca68425f85f831bd265108f
Vipul-Tandon <vipul.tandon@metafic.co>
2024-01-15 10:43:25 +0530

    Practised predicate enumerable methods

--- Ruby/Day 4/10.predicate_enumerable_methods.rb
+++ Ruby/Day 4/10.predicate_enumerable_methods.rb
@@ -3,37 +3,37 @@
 ## a) all?: Checks if a given block returns true for all elements in the collection.
 numbers = [1, 2, 3, 4, 5]
 result = numbers.all? { |n| n > 0 }
-puts result  # Output: true
+puts result                                 # true
 
 
 ## b) any?: Checks if a given block returns true for at least one element in the collection.
 numbers = [1, 2, 3, 4, 5]
 result = numbers.any? { |n| n < 0 }
-puts result  # Output: false
+puts result                                 # false
 
 
 ## c) none?: Checks if none of the elements in the collection satisfy the given block.
 numbers = [1, 2, 3, 4, 5]
 result = numbers.none? { |n| n < 0 }
-puts result  # Output: true
+puts result                                 # true
 
 
 ## d) one?: Checks if exactly one element in the collection satisfies the given block.
 numbers = [1, 2, 3, 4, 5]
 result = numbers.one? { |n| n == 3 }
-puts result  # Output: true
+puts result                                 # true
 
 
 ## e) count: Returns the number of elements in the collection that satisfy the given block.
 numbers = [1, 2, 3, 4, 5]
 count = numbers.count { |n| n.even? }
-puts count  # Output: 2
+puts count                                  # 2
 
 
 ## f) include?: Checks if the collection includes a specific element.
 fruits = ["apple", "banana", "orange"]
 result = fruits.include?("banana")
-puts result  # Output: true
+puts result                                 # true
 
 
 ## g) member?: An alias for `include`?.
@@ -41,17 +41,18 @@ puts result  # Output: true
 ## h) find: Returns the first element in the collection that satisfies the given block.
 numbers = [1, 2, 3, 4, 5]
 result = numbers.find { |n| n.even? }
-puts result  # Output: 2
+puts result                                 # 2
 
 
 ## i) find_index: Returns the index of the first element that satisfies the given block.
 numbers = [1, 2, 3, 4, 5]
 index = numbers.find_index { |n| n == 3 }
-puts index  # Output: 2
+puts index                                  # 2
 
 
 ## j) grep: Returns an array of elements that match a given pattern or regular expression.
-numbers = [1, 2, 3, 4, 5]
-index = numbers.find_index { |n| n == 3 }
-puts index  # Output: 2
+words = ["apple", "banana", "orange", "grape"]
+filtered_words = words.grep(/a/)
+print filtered_words                         # ["apple", "banana", "grape"]
+
 
